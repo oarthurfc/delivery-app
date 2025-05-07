0850338ccf683b316713eb0c8bbf80214a8b38fd
oarthurfc <arthuryouface@gmail.com>
2025-05-07 09:32:25 -0300

    feat: adiciona nav bar inferior

---
+++ lib/widgets/common/app_bar_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final void Function(int) onTap;
+
+  const AppBottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Configurações',
+        ),
+      ],
+    );
+  }
+}
