8698c6eadd71da253290855a2f424afbe95810b4
oarthurfc <arthuryouface@gmail.com>
2025-06-16 11:19:23 -0300

    fix: correcting folder name

--- backend/eureka-server/src/main/java/com/delivery_app/eureka_server/EurekaServerApplication.java
+++ backend/eureka-server/src/main/java/com/delivery_app/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.dalivery_app.eureka_server;
+package com.delivery_app.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
