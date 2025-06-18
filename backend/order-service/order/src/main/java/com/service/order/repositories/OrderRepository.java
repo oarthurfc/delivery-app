0c5b61943bbda135f11dfc270ce4f7d4c3f0c54d
oarthurfc <arthuryouface@gmail.com>
2025-06-18 16:32:40 -0300

    feat: implementing get order by driverId

--- backend/order-service/order/src/main/java/com/service/order/repositories/OrderRepository.java
+++ backend/order-service/order/src/main/java/com/service/order/repositories/OrderRepository.java
@@ -2,8 +2,13 @@ package com.service.order.repositories;
 
 import com.service.order.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByDriverId(Long driverId);
+    Page<Order> findByDriverId(Long driverId, Pageable pageable);
 }
